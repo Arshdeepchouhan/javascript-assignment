4666e7cfd9f3a9263bae7c0f2b86708378400467
Arshdeepchouhan <156530538+Arshdeepchouhan@users.noreply.github.com>
2025-02-07 00:02:53 -0500

    Add files via upload

---
+++ readFile.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+fs.readFile('./sample.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+  console.log('File contents:');
+  console.log(data);
+});
